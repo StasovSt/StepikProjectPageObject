53540dcc92e5d5e13ba6980c2bd8ea1a08f437ac
Anisimov Stas <stsv.sts@gmail.com>
2022-12-09 04:23:16 +0300

    Первый тест на основе Page Object + правка base_pages.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,6 @@
-
 class BasePage():
-    def __int__(self, browser, url):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-
     def open(self):
         self.browser.get(self.url)
