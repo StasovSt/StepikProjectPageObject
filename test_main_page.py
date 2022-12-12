82cd6dd9b0f29703cf83701020eb84f9c14299e1
Anisimov Stas <stsv.sts@gmail.com>
2022-12-12 05:27:34 +0300

    Подредачил тест на проверку наличия формы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
-import pytest
+"""Start test
+pytest -v --tb=line --language=en test_main_page.py
+"""
 
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -17,6 +20,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.skip
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page_login = LoginPage(browser, link)
@@ -24,11 +28,15 @@ def test_guest_can_see_login_form(browser):
     page_login.should_be_login_form()
 
 def test_guest_can_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page_login = LoginPage(browser, link)
-    page_login.open()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    page_login = LoginPage(browser, browser.current_url)
     page_login.should_be_register_form()
 
+@pytest.mark.skip
 def test_login_url_is_corrected(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page_login = LoginPage(browser, link)
