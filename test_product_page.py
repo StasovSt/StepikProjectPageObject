558d407e63330b60431a8cdd28b4767d81eea44f
Stasovss <stsv.sts@gmail.com>
2023-03-09 18:29:57 +0300

    в test_product_page.py добавлен старый тест test_guest_can_add_product_to_basket
    часть тестов отмаркирована need_review

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@
 Тесты для работы с конкретным продуктом
 Start test
 pytest -v --tb=line --language=en test_product_page.py
-
+pytest -v --tb=line --language=en -m need_review test_product_page.py
 Если вам нужна дополнительная информация, использовать -v
 """
 import pytest
@@ -16,6 +16,7 @@ from .pages.login_page import LoginPage
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
+        """Setup-функция производит авторизацию пользователя перед тестами"""
         link = "http://selenium1py.pythonanywhere.com/ru/"
         email = str(time.time()) + "@fakemail.org"
         password = str(time.time())
@@ -29,8 +30,7 @@ class TestUserAddToBasketFromProductPage():
 
         page.should_be_authorized_user()
 
-
-    # @pytest.mark.xfail
+    @pytest.mark.xfail
     def test_user_cant_see_success_message(self, browser):
         """Пользователь не видит сообщение об успешном добавлении"""
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -40,8 +40,7 @@ class TestUserAddToBasketFromProductPage():
         page_basket = ProductPage(browser, link)
         page_basket.should_not_be_success_message_not_present()
 
-    # @pytest.mark.parametrize('endpoint', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-    # @pytest.mark.skip
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         """Проверка добавления товара в корзину
         Пользователь видит сообщение об успешном добавлении"""
@@ -61,8 +60,6 @@ class TestUserAddToBasketFromProductPage():
         # Проверка что стоимость корзины равна стоимости товара
         page_basket.check_value_price_basket()
 
-
-@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -74,8 +71,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
     page_basket.should_not_be_success_message_not_present()
 
-
-@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -87,24 +82,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
     page_basket.should_not_be_success_message_is_disappeared()
 
-
-@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
-
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
 
-
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = BasketPage(browser, link)
@@ -113,3 +104,24 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
     page.basket_is_empty_not_products()
     page.basket_is_empty_message()
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('endpoint', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, endpoint):
+    """Проверка добавления товара в корзину
+    Пользователь видит сообщение об успешном добавлении"""
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{endpoint}"
+    page = BasePage(browser, link)
+    page.open()
+
+    page_basket = ProductPage(browser, link)
+
+    # Чек названия продукта и его цены
+    page_basket.add_product_into_the_basket()  # добавить продукт в корзину
+    page_basket.solve_quiz_and_get_code()  # Работа с аллертом
+    # Проверка что товар добавлен в корзину
+    page_basket.product_in_basket_check()
+
+    # Проверка что стоимость корзины равна стоимости товара
+    page_basket.check_value_price_basket()
+
